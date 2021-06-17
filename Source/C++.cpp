0bf014ff6e07bf7d86b76ec68ea29d337a8099f8
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-17 15:47:10 +0530

    Optimised C++ program even more

--- Source/C++.cpp
+++ Source/C++.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int main() {
-	long long int currentFibonacci = 0;
-	long long int prevFibonacci1 = 1;
-	long long int prevFibonacci2 = 0;
+	unsigned long long int currentFibonacci = 0;
+	unsigned long long int prevFibonacci1 = 1;
+	unsigned long long int prevFibonacci2 = 0;
 
 	while(true) {
 		currentFibonacci = prevFibonacci1 + prevFibonacci2;
 
-		if(currentFibonacci < 0) break;
+		if(currentFibonacci < prevFibonacci1) break;
 
 		std::cout << currentFibonacci << "\n";
 
