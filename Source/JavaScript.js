23bf4b4e22f2332774ec15e0bb4a7a70e822fd66
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-19 11:13:23 +0530

    Added Timeout-y code for JS & TS

--- Source/JavaScript.js
+++ Source/JavaScript.js
@@ -11,3 +11,14 @@ while(true) {
 }
 
 // Goes on till infinity, or atleast till we continue the program or the memory goes booom
+
+// The following code is kinda better, use this one if you want to run the code after some fixed time, like every second or 2nd second etc
+
+// setInterval(() => {
+// 	currentFibonacci = prevFibonacci1 + prevFibonacci2;
+
+// 	console.log(`${currentFibonacci.toString()}\n`);
+// 	prevFibonacci2 = prevFibonacci1;
+// 	prevFibonacci1 = currentFibonacci;
+// }, 1000)
+// Timeout is in Miliseconds, so set it accordingly
--- Source/TypeScript.ts
+++ Source/TypeScript.ts
@@ -1,3 +1,4 @@
+// For this to work, you need to have Lib option to be set to ESNext or ES2020 (Might accept others too but I am not sure, but it doesn't support ES5, I'm sure about that)
 let prevFibonacci2: bigint = BigInt("0");
 let prevFibonacci1: bigint = BigInt("1");
 let currentFibonacci: bigint = BigInt("0");
@@ -11,3 +12,14 @@ while(true) {
 }
 
 // Goes on till infinity, or atleast till we continue the program or the memory goes booom
+
+// The following code is kinda better, use this one if you want to run the code after some fixed time, like every second or 2nd second etc
+
+// setInterval(() => {
+// 	currentFibonacci = prevFibonacci1 + prevFibonacci2;
+
+// 	console.log(`${currentFibonacci.toString()}\n`);
+// 	prevFibonacci2 = prevFibonacci1;
+// 	prevFibonacci1 = currentFibonacci;
+// }, 1000);
+// Timeout is in Miliseconds, so set it accordingly
