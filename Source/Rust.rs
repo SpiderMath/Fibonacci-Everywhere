352fc6ff5876010b60c12b56cf91a1dd53a19bbc
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-19 08:36:12 +0530

    Added Fibonacci in Rust

---
+++ Source/Rust.rs
@@ -0,0 +1,17 @@
+fn main() {
+	// I know we can use the external crate num and use BigInt but that needs a project setup and whatnot, 
+	// so I am using u128 as the 2nd largest option [It isn't too large though]
+	// This program breaks on overflow on its own, so no tension
+	let mut current_fibonacci: u128;
+	let mut previous_fibonacci_1: u128 = 1;
+	let mut previous_fibonacci_2: u128 = 0;
+
+	loop {
+		current_fibonacci = previous_fibonacci_1 + previous_fibonacci_2;
+
+		println!("{}\n", current_fibonacci);
+
+		previous_fibonacci_1 = previous_fibonacci_2;
+		previous_fibonacci_2 = current_fibonacci;
+	}
+}
