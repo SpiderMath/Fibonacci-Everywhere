204c1622119ae9631c039c0104fcb77928612890
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-21 23:05:48 +0530

    Added sleep in Ruby, how crazy is that

--- Source/Ruby.rb
+++ Source/Ruby.rb
@@ -2,6 +2,8 @@ previousFibonacci1 = 1
 previousFibonacci2 = 0
 
 while true
+	# Uncomment the following line of code, if you want to like make the program sleep for a sec before going on to the next one. The parameter takes in Seconds BTW
+	# sleep(1)
 	currentFibonacci = previousFibonacci1 + previousFibonacci2
 
 	puts currentFibonacci
