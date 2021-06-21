7c832146534a374f8d3835a9603eb9387f3ce1ab
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-21 22:59:03 +0530

    Forgot to add the \n

--- Source/Ruby.rb
+++ Source/Ruby.rb
@@ -5,6 +5,7 @@ while true
 	currentFibonacci = previousFibonacci1 + previousFibonacci2
 
 	puts currentFibonacci
+	puts "\n"
 
 	previousFibonacci2 = previousFibonacci1
 	previousFibonacci1 = currentFibonacci
