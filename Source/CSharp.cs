12d152f542d74efbb03cc885acdadfe99d1c56df
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-23 18:24:33 +0530

    Added Timeout in C#

--- Source/CSharp.cs
+++ Source/CSharp.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Numerics;
+// Uncomment the commented out code to wait for a bit to generate the new number
+// using System.Threading;
 
-class Fibonacci
+public class Fibonacci
 {
-	static void Main()
+	public static void Main()
 	{
 		BigInteger previousFibonacci1 = new BigInteger(1);
 		BigInteger previousFibonacci2 = new BigInteger(0);
 
 		while (true)
 		{
+			// Takes in time in Miliseconds. Uncomment the following line if you want the program to wait for a while before continuing
+			// Thread.Sleep(1000);
 			BigInteger currentFibonacci = BigInteger.Add(previousFibonacci1, previousFibonacci2);
 
 			Console.WriteLine(currentFibonacci.ToString("R") + "\n");
