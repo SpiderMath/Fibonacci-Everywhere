348ddb4d0cd0ece5607b9681b6516c64dcbb6aa6
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-25 12:18:48 +0530

    Improved timeout in JavaScript as well :grin:

--- Source/JavaScript.js
+++ Source/JavaScript.js
@@ -2,7 +2,16 @@ let prevFibonacci2 = BigInt("0");
 let prevFibonacci1 = BigInt("1");
 let currentFibonacci = BigInt("0");
 
+// Uncomment the following code, to let your computer rest a bit before continuing
+// function sleep(ms) {
+// 	const start = new Date().getTime();
+// 	while(new Date().getTime() - start < ms) continue;
+// }
+
 while(true) {
+	// Uncomment the following line too, if you want the timeout to work. Remember that the input is in MILISECONDS not seconds
+	// sleep(1000);
+
 	currentFibonacci = prevFibonacci1 + prevFibonacci2;
 
 	console.log(`${currentFibonacci.toString()}\n`);
@@ -11,14 +20,3 @@ while(true) {
 }
 
 // Goes on till infinity, or atleast till we continue the program or the memory goes booom
-
-// The following code is kinda better, use this one if you want to run the code after some fixed time, like every second or 2nd second etc
-
-// setInterval(() => {
-// 	currentFibonacci = prevFibonacci1 + prevFibonacci2;
-
-// 	console.log(`${currentFibonacci.toString()}\n`);
-// 	prevFibonacci2 = prevFibonacci1;
-// 	prevFibonacci1 = currentFibonacci;
-// }, 1000)
-// Timeout is in Miliseconds, so set it accordingly
