d06c2ca6dfac223e06fff1854b801325ebefc0bb
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-25 12:16:02 +0530

    Improved cooldowns in TypeScript

--- Source/TypeScript.ts
+++ Source/TypeScript.ts
@@ -3,7 +3,15 @@ let prevFibonacci2: bigint = BigInt("0");
 let prevFibonacci1: bigint = BigInt("1");
 let currentFibonacci: bigint = BigInt("0");
 
+// Uncomment the commented out code, if you require your computer to wait a bit before continuing
+// function sleep(ms: number) {
+// 	const start = new Date().getTime();
+// 	while(new Date().getTime() - start < ms) continue;
+// }
+
 while(true) {
+	// The parameter takes in miliseconds
+	// sleep(1000);
 	currentFibonacci = prevFibonacci1 + prevFibonacci2;
 
 	console.log(`${currentFibonacci.toString()}\n`);
@@ -12,14 +20,3 @@ while(true) {
 }
 
 // Goes on till infinity, or atleast till we continue the program or the memory goes booom
-
-// The following code is kinda better, use this one if you want to run the code after some fixed time, like every second or 2nd second etc
-
-// setInterval(() => {
-// 	currentFibonacci = prevFibonacci1 + prevFibonacci2;
-
-// 	console.log(`${currentFibonacci.toString()}\n`);
-// 	prevFibonacci2 = prevFibonacci1;
-// 	prevFibonacci1 = currentFibonacci;
-// }, 1000);
-// Timeout is in Miliseconds, so set it accordingly
